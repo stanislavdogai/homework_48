b27f13aa4cefbef9ee880b8dad8350a3586723bb
stanislavdogai <stanislavdogai01@gmail.com>
2022-01-10 02:26:14 +0600

    Создана главная страница и детальный просмотр каждого продукта

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import home_page
+from webapp.views import home_page, product_view
 
 urlpatterns = [
-    path('', home_page, name='home_page')
+    path('', home_page, name='home_page'),
+    path('product/<int:pk>/', product_view, name='product_view')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from webapp.models import Product
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    products = Product.objects.order_by('name')
+    return render(request, 'index.html', {'products' : products})
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product' : product}
+    return render(request, 'product_view.html', context)
