3f1f41119cfdfca1143be825ec16595738398ebb
stanislavdogai <stanislavdogai01@gmail.com>
2022-01-10 01:48:33 +0600

    зарегистрирована модель в админ панели, созданы образцы

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product
+#
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'name']
+#     list_filter = ['id']
+#     fields = ['id', 'name', 'remainder', 'price']
+
+
+admin.site.register(Product)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY = [('none', 'Разное'), ('milk', 'Молочная продукция'), ('meat', 'МЯСО'), ('vegetables', 'Овощи'), ('drink', 'Напитки')]
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Описание')
+    category = models.CharField(max_length=15, default='other', choices=CATEGORY, verbose_name='Категория')
+    remainder = models.PositiveIntegerField(null=False, blank=False, verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=False, null=False, verbose_name='Цена')
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
