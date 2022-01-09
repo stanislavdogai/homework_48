0a1eb2379c9a6722f3b11bf325bb0d3ed0a4e869
stanislavdogai <stanislavdogai01@gmail.com>
2022-01-10 02:54:35 +0600

    Добавлен метод создания продукта

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import home_page, product_view
+from webapp.views import home_page, product_view, create_product
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('product/<int:pk>/', product_view, name='product_view')
+    path('product/<int:pk>/', product_view, name='product_view'),
+    path('create/', create_product, name='create_product')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Product
+from webapp.forms import ProductForm
 # Create your views here.
 def home_page(request):
     products = Product.objects.order_by('name')
@@ -9,3 +10,19 @@ def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product' : product}
     return render(request, 'product_view.html', context)
+
+def create_product(request):
+    if request.method == 'GET':
+        form = ProductForm()
+        return render(request, 'create.html', {'form' : form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            category = form.cleaned_data.get('category')
+            price = form.cleaned_data.get('price')
+            remainder = form.cleaned_data.get('remainder')
+            new_product = Product.objects.create(name=name,description=description,category=category,price=price,remainder=remainder)
+            return redirect('product_view', pk=new_product.pk)
+        return render(request, 'create.html', {'form' : form})
